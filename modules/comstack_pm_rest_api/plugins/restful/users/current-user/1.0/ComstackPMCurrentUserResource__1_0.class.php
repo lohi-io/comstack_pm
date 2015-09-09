50597d8aea4dde8b19da1b70414257740496c9c5
Alli Price <allister.price@gmail.com>
2015-09-09 15:59:53 +0000

    Added the new current user endpoint.

---
+++ modules/comstack_pm_rest_api/plugins/restful/users/current-user/1.0/ComstackPMCurrentUserResource__1_0.class.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @file
+ * Contains ComstackPMCurrentUserResource__1_0.
+ */
+
+class ComstackPMCurrentUserResource__1_0 extends \ComstackPMUsersResource__1_0 {
+  /**
+   * Overrides \RestfulDataProviderEFQ::controllersInfo().
+   */
+  public static function controllersInfo() {
+    return array(
+      '' => array(
+        \RestfulInterface::GET => 'getUser',
+      ),
+    );
+  }
+
+  /**
+   * Overrides \RestfulBase::isListRequest().
+   *
+   * This endpoint will never return a list, ever!
+   */
+  public function isListRequest() {
+    return FALSE;
+  }
+
+  /**
+   * Return information on the current user.
+   */
+  public function getUser() {
+    $account = $this->getAccount();
+
+    $user = $this->viewEntity($account->uid);
+    $permissions = array(
+      'conversations' => array(
+        'start' => user_access('start new comstack conversations', $account),
+        'leave' => user_access('delete leave comstack conversation', $account),
+        'reply' => user_access('reply to a comstack conversation', $account),
+        'invite_others' => user_access('invite users to a comstack conversation', $account),
+        'set_title' => user_access('set a comstack conversations title', $account),
+        'mark_as_read' => user_access('mark a comstack conversation as read', $account),
+        'mute' => user_access('mute a comstack conversation', $account),
+        'archive' => user_access('archive a comstack conversation', $account),
+        'pin' => user_access('pin a comstack conversation', $account),
+        'star' => user_access('star a comstack conversation', $account),
+      ),
+      'messages' => array(
+        'edit_own' => user_access('edit own comstack conversation messages', $account),
+        'delete' => user_access('delete own comstack conversation messages', $account),
+      ),
+    );
+
+    return array(
+      'user' => $user,
+      'permissions' => $permissions,
+    );
+  }
+}
