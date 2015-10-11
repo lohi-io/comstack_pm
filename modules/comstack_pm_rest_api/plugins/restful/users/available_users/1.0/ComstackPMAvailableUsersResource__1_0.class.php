b982f21c0455bdc4c9ba6376aea20fc3b291bc61
Alli Price <allister.price@gmail.com>
2015-10-11 10:54:42 +0000

    Update the available users endpoint autocomplete behaviour to return full objects instead of just ids.

--- modules/comstack_pm_rest_api/plugins/restful/users/available_users/1.0/ComstackPMAvailableUsersResource__1_0.class.php
+++ modules/comstack_pm_rest_api/plugins/restful/users/available_users/1.0/ComstackPMAvailableUsersResource__1_0.class.php
@@ -85,4 +85,61 @@ class ComstackPMAvailableUsersResource__1_0 extends \ComstackUsersResource__1_0 
 
     return $query;
   }
+
+  /**
+   * Overrides \RestfulEntityBase::getQueryResultForAutocomplete().
+   */
+  protected function getQueryResultForAutocomplete() {
+    $entity_type = $this->getEntityType();
+    $query = $this->getQueryForAutocomplete();
+    $result = $query->execute();
+
+    if (empty($result[$entity_type])) {
+      // No entities found.
+      return array();
+    }
+
+    $ids = array_keys($result[$entity_type]);
+
+    // Pre-load all entities if there is no render cache.
+    $cache_info = $this->getPluginKey('render_cache');
+    if (!$cache_info['render']) {
+      entity_load($entity_type, $ids);
+    }
+
+    $return = array();
+
+    // If no IDs were requested, we should not throw an exception in case an
+    // entity is un-accessible by the user.
+    foreach ($ids as $id) {
+      if ($row = $this->viewEntity($id)) {
+        $return[] = $row;
+      }
+    }
+
+    return $return;
+  }
+
+  /**
+   * Overrides \RestfulEntityBase::getListForAutocomplete().
+   */
+  protected function getListForAutocomplete() {
+    $entity_info = $this->getEntityInfo();
+    if (empty($entity_info['entity keys']['label'])) {
+      // Entity is invalid for autocomplete, as it doesn't have a "label"
+      // property.
+      $params = array('@entity' => $this->getEntityType());
+      throw new \Exception(format_string('Cannot autocomplete @entity as it does not have a "label" property defined.', $params));
+    }
+
+    $request = $this->getRequest();
+    if (empty($request['autocomplete']['string'])) {
+      // Empty string.
+      return array();
+    }
+
+    $return = $this->getQueryResultForAutocomplete();
+
+    return $return;
+  }
 }
