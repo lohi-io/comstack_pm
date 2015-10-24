7eedce32be93592614a69db3446478cb2bd6d8c4
Alli <allister.price@gmail.com>
2015-10-24 23:01:44 +0000

    Update current user endpoint to use the new report permission.

--- modules/comstack_pm_rest_api/plugins/restful/users/current_user/1.0/ComstackPMCurrentUserResource__1_0.class.php
+++ modules/comstack_pm_rest_api/plugins/restful/users/current_user/1.0/ComstackPMCurrentUserResource__1_0.class.php
@@ -46,7 +46,7 @@ class ComstackPMCurrentUserResource__1_0 extends \ComstackUsersResource__1_0 {
           'archive' => user_access('archive a comstack conversation', $account),
           'pin' => user_access('pin a comstack conversation', $account),
           'star' => user_access('star a comstack conversation', $account),
-          'report' => user_access('flag comstack_pm_report', $account),
+          'report' => user_access('create comstack_pm_report entries', $account),
         ),
         'messages' => array(
           'edit_own' => user_access('edit own comstack conversation messages', $account),
