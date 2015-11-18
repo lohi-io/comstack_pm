241dcc414fc10fcc8f8e8361d873ee40e84477f0
Alli <allister.price@gmail.com>
2015-11-18 11:54:21 +0000

    Updated the available user endpoint permissions to be more exact about how to behave/what a user can do when read only modes are active.

--- modules/comstack_pm_rest_api/plugins/restful/users/current_user/1.0/ComstackPMCurrentUserResource__1_0.class.php
+++ modules/comstack_pm_rest_api/plugins/restful/users/current_user/1.0/ComstackPMCurrentUserResource__1_0.class.php
@@ -31,32 +31,34 @@ class ComstackPMCurrentUserResource__1_0 extends \ComstackUsersResource__1_0 {
    */
   public function getUser() {
     $account = $this->getAccount();
+
     $forced_read_only = (bool) variable_get('comstack_pm_killswitch__enabled', FALSE);
     $user_pm_enabled = $forced_read_only ? FALSE : user_preferences($account->uid, 'comstack_pm_enabled');
+    $friends_read_only = variable_get('comstack_friends_killswitch__enabled', FALSE);
 
     $data = array(
       'user' => $this->viewEntity($account->uid),
       'permissions' => array(
         'conversations' => array(
-          'start' => user_access('start new comstack conversations', $account) && $user_pm_enabled,
-          'leave' => user_access('delete leave comstack conversation', $account),
-          'reply' => user_access('reply to a comstack conversation', $account) && $user_pm_enabled,
-          'invite_others' => user_access('invite users to a comstack conversation', $account) && $user_pm_enabled,
-          'set_title' => user_access('set a comstack conversations title', $account) && $user_pm_enabled,
+          'start' => $user_pm_enabled && user_access('start new comstack conversations', $account),
+          'leave' => $user_pm_enabled && user_access('delete leave comstack conversation', $account),
+          'reply' => $user_pm_enabled && user_access('reply to a comstack conversation', $account),
+          'invite_others' => $user_pm_enabled && user_access('invite users to a comstack conversation', $account),
+          'set_title' => $user_pm_enabled && user_access('set a comstack conversations title', $account),
           'mark_as_read' => user_access('mark a comstack conversation as read', $account),
           'mute' => user_access('mute a comstack conversation', $account),
           'archive' => user_access('archive a comstack conversation', $account),
           'pin' => user_access('pin a comstack conversation', $account),
           'star' => user_access('star a comstack conversation', $account),
-          'report' => user_access('create comstack_pm_report entries', $account),
+          'report' => $user_pm_enabled && user_access('create comstack_pm_report entries', $account),
         ),
         'messages' => array(
-          'edit_own' => user_access('edit own comstack conversation messages', $account) && $user_pm_enabled,
-          'delete' => user_access('delete own comstack conversation messages', $account),
+          'edit_own' => $user_pm_enabled && user_access('edit own comstack conversation messages', $account),
+          'delete' => $user_pm_enabled && user_access('delete own comstack conversation messages', $account),
         ),
         'users' => array(
-          'friend' => user_access('can request comstack_friends relationships', $account),
-          'block' => user_access('can request comstack_blocked relationships', $account),
+          'friend' => !$friends_read_only && user_access('can request comstack_friends relationships', $account),
+          'block' => !$friends_read_only && user_access('can request comstack_blocked relationships', $account),
         ),
       ),
       'preferences' => array(
