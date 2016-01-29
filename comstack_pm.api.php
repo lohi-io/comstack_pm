d4e46801280f62b3d035f98f86dab266490cbb8d
heylookalive <allister.price@gmail.com>
2016-01-29 17:16:54 +0000

    Add action to delete a conversation.

--- comstack_pm.api.php
+++ comstack_pm.api.php
@@ -16,3 +16,10 @@ function hook_comstack_conversation_mark_as_read($conversation, $acting_user) {
  */
 function hook_comstack_conversation_reply($conversation, $message, $acting_user) {
 }
+
+/**
+ * Implements hook_comstack_pm_conversations_permanently_deleted().
+ */
+function hook_comstack_pm_conversations_permanently_deleted($conversation_ids) {
+
+}
